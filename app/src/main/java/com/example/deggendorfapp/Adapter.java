ce093df7669a6a68ad6e5ecc1ee2983d244bf262
Chaitanya <schlockmangra@gmail.com>
2022-02-16 10:30:41 +0100

    Chaitanya Mangra

--- app/src/main/java/com/example/deggendorfapp/Adapter.java
+++ app/src/main/java/com/example/deggendorfapp/Adapter.java
@@ -1,26 +1,72 @@
 package com.example.deggendorfapp;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.recyclerview.widget.RecyclerView;
+public class Adapter extends RecyclerView.Adapter<Adapter.NumberViewHolder> {
 
-public class Adapter extends RecyclerView.Adapter {
-
-
-
-    public class AdapterViewHolder extends RecyclerView.ViewHolder {
-        public final TextView mWeatherTextView = null;
+    private static final String TAG = Adapter.class.getSimpleName();
+    private int mNumberItems; //number of items to display in list
 
 
-        public AdapterViewHolder(View view) {
-            super(view);
+    public Adapter(int mNumberItems) {
+        this.mNumberItems = mNumberItems;
+    }
 
 
+    /*
+    viewGroup:  The ViewGroup that these ViewHolders are contained within.
+    viewType:   If your RecyclerView has more than one type of item (which ours doesn't) you
+                can use this viewType integer to provide a different layout. See
+                {@link android.support.v7.widget.RecyclerView.Adapter#getItemViewType(int)}
+                for more details.
+    return:     A new NumberViewHolder that holds the View for each list item
+    */
+
+    @Override
+    public NumberViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.items_for_recycler_view;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        NumberViewHolder viewHolder = new NumberViewHolder(view);
+
+        return viewHolder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(NumberViewHolder holder, int position) {
+        Log.d(TAG, "#" + position);
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNumberItems;
+    }
+
+    class NumberViewHolder extends RecyclerView.ViewHolder {
+        TextView listItemNumberView;
+
+        public NumberViewHolder(View itemView) {
+            super(itemView);
+
+            listItemNumberView = (TextView) itemView.findViewById(R.id.tv_item_number);
+        }
+
+        void bind(int listIndex) {
+            listItemNumberView.setText(String.valueOf(listIndex));
         }
     }
+
 }
--- app/src/main/java/com/example/deggendorfapp/MainActivity.java
+++ app/src/main/java/com/example/deggendorfapp/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.deggendorfapp;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -14,65 +11,47 @@ import androidx.recyclerview.widget.RecyclerView;
 public class MainActivity extends AppCompatActivity {
 
 
-    RecyclerView mRecyclerView;
+    private RecyclerView mRecyclerView;
     TextView mErrorMessageDisplay;
     ProgressBar mLoadingIndicator;
-    Adapter mAdapter;
+    private Adapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_forecast);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_items);
 
         /* This TextView is used to display errors and will be hidden if there are no errors */
-        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
+        // mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
 
         /*
          * LinearLayoutManager can support HORIZONTAL or VERTICAL orientations. The reverse layout
          * parameter is useful mostly for HORIZONTAL layouts that should reverse for right to left
          * languages.
          */
-        LinearLayoutManager layoutManager
-                = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
 
 
         mRecyclerView.setLayoutManager(layoutManager);
 
+    }
+}
+/*
+ * Use this setting to improve performance if you know that changes in content do not
+ * change the child layout size in the RecyclerView
+ */
+      /*  mRecyclerView.setHasFixedSize(true);
 
-        /*
-         * Use this setting to improve performance if you know that changes in content do not
-         * change the child layout size in the RecyclerView
-         */
-        mRecyclerView.setHasFixedSize(true);
 
-        /*
-         * The ForecastAdapter is responsible for linking our weather data with the Views that
-         * will end up displaying our weather data.
-         */
-        //mAdapter = new Adapter(this);
-
-        /* Setting the adapter attaches it to the RecyclerView in our layout. */
-        //mRecyclerView.setAdapter(mAdapter);
-
-        /*
-         * The ProgressBar that will indicate to the user that we are loading data. It will be
-         * hidden when no data is loading.
-         *
-         * Please note: This so called "ProgressBar" isn't a bar by default. It is more of a
-         * circle. We didn't make the rules (or the names of Views), we just follow them.
-         */
         mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
 
         loadData();
 
-    }
 
 
-    private void loadData() {
+    /*private void loadData() {
         showData();
 
 
@@ -88,15 +67,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
-        MenuInflater inflater = getMenuInflater();
+      /*  MenuInflater inflater = getMenuInflater();
         /* Use the inflater's inflate method to inflate our menu layout to this menu */
-        inflater.inflate(R.menu.menu, menu);
+        /*inflater.inflate(R.menu.menu, menu);
         /* Return true so that the menu is displayed in the Toolbar */
-        return true;
+        /*return true;
     }
-
-
-
-
-
-}
+/*
